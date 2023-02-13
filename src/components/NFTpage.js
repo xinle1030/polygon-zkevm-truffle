716c1458ee9f3f1249ec7c599589c22df4c9613d
LAMXINLE <xlam0003@student.monash.edu>
2023-02-13 21:55:10 +0800

    update min-height to minHeight

--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -88,7 +88,7 @@ export default function NFTPage(props) {
   if (!dataFetched) getNFTData(tokenId);
 
   return (
-    <div style={{ "min-height": "100vh" }}>
+    <div style={{ "minHeight": "100vh" }}>
       <Navbar></Navbar>
       <div className="flex ml-20 mt-20">
         <img src={data.image} alt="" className="w-2/5" />
