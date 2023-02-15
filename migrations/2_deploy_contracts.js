36905942c932c789ead81ce8b5f2f3f5d5c5dc52
LAMXINLE <xlam0003@student.monash.edu>
2023-02-15 17:16:01 +0800

    update contract migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
-// const ZkERC20 = artifacts.require("./zkERC20.sol");
-const NFTMarketplace = artifacts.require("./NFTMarketplace.sol");
+const RWDToken = artifacts.require("RWDToken");
+const NFTMarketplace = artifacts.require("NFTMarketplace");
 
-module.exports = function (deployer) {
-  // deployer.deploy(ZkERC20);
-  deployer.deploy(NFTMarketplace);
+module.exports = async function (deployer) {
+  await deployer.deploy(RWDToken);
+  const rwdToken = await RWDToken.deployed();
+
+  await deployer.deploy(NFTMarketplace, rwdToken.address);
+  const nftMarketplace = await NFTMarketplace.deployed();
+
+  // Transfer all RWD tokens to Marketplace (1 million)
+  // await rwdToken.transfer(nftMarketplace.address, "100000000000000000000000");
 };
