ee754ef3da78ad7c9e902bf46dd9c1abe00cc1b4
LAMXINLE <xlam0003@student.monash.edu>
2023-02-15 17:15:47 +0800

    Complete reward token contract

--- contracts/RWDToken.sol
+++ contracts/RWDToken.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ZkERC20 is ERC20, Ownable {
-    constructor() ERC20("zkERC20", "ZK20") {
+contract RWDToken is ERC20, Ownable {
+    constructor() ERC20("RWDToken", "RDW") {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
