4dd0df60e6d0c7008a1e14fb17670ac35704ca28
LAMXINLE <xlam0003@student.monash.edu>
2023-02-16 13:34:15 +0800

    update deployment of nftMarketplace

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,11 @@ const RWDToken = artifacts.require("RWDToken");
 const NFTMarketplace = artifacts.require("NFTMarketplace");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(RWDToken);
-  const rwdToken = await RWDToken.deployed();
+  // await deployer.deploy(RWDToken);
+  // const rwdToken = await RWDToken.deployed();
+  let rwdTokenAddr = "0x1D0B434dD82280AD64F4ED50867156c068eCbB99";;
 
-  await deployer.deploy(NFTMarketplace, rwdToken.address);
+  await deployer.deploy(NFTMarketplace, rwdTokenAddr);
   const nftMarketplace = await NFTMarketplace.deployed();
 
   // Transfer all RWD tokens to Marketplace (1 million)
