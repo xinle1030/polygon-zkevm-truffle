6149f53ceb0c5524bd2b6378ec1db75e112a0489
LAMXINLE <xlam0003@student.monash.edu>
2023-02-16 16:25:06 +0800

    remove unused comment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,6 @@ const NFTMarketplace = artifacts.require("NFTMarketplace");
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(RWDToken);
   const rwdToken = await RWDToken.deployed();
-  // let rwdTokenAddr = "0x1D0B434dD82280AD64F4ED50867156c068eCbB99";
 
   await deployer.deploy(NFTMarketplace, rwdToken.address);
   const nftMarketplace = await NFTMarketplace.deployed();
